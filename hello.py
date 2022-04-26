4d865e95ea499f7859284848ae4941eeeb6a0586
helmi3044 <helmimansur88@gmail.com>
2022-04-26 15:08:02 +0700

    modified files

--- hello.py
+++ hello.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+import numpy as np
+import pickle
+
+model = pickle.load(open("model/model_RFRegr_FP1.pkl", "rb"))
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template('index.html')
+
+# Redirecting the API to predict the result
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    """
+    For Rendering result on HTML GUI
+    """
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template("index.html", prediction_text="Prediksi Harga Taksi Adalah : {}".format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
